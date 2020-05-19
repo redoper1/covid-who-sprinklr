5568b813b801a3f55030f62b893c87dbd9e70900
David Rychlý <davidrychly2@gmail.com>
2020-05-19 22:33:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# WHO Coronavirus stats
+
+Actor gets an actual number of examined and infected people from https://dashboards-dev.sprinklr.com/data/9043/global-covid19-who-gis.json
+
+Latest data are available at this URL: .
+
+You can find a dataset with unique history items at this URL: .
+
+Website is being scraped every 5 minutes.
+
+Scraper has been created by [BOOT!Q](https://bootiq.io/).
---
+++ src/main.js
@@ -0,0 +1,114 @@
+const Apify = require('apify');
+const {log, requestAsBrowser} = Apify.utils;
+const {getName:countryName} = require('i18n-iso-countries');
+
+const LATEST = "LATEST";
+
+Apify.main(async () => {
+    const { notificationEmail } = await Apify.getInput();
+    const url = "https://dashboards-dev.sprinklr.com/data/9043/global-covid19-who-gis.json";
+    const kvStore = await Apify.openKeyValueStore("COVID-19-WHO-SPRINKLR");
+    const dataset = await Apify.openDataset("COVID-19-WHO-SPRINKLR-HISTORY");
+    const requestList = await Apify.openRequestList('LIST', [
+        {
+            url: 'https://dashboards-dev.sprinklr.com/data/9043/global-covid19-who-gis.json'
+        }
+    ])
+
+    if (notificationEmail) {
+        await Apify.addWebhook({
+            eventTypes: ['ACTOR.RUN.FAILED', 'ACTOR.RUN.TIMED_OUT'],
+            requestUrl: `https://api.apify.com/v2/acts/mnmkng~email-notification-webhook/runs?token=${Apify.getEnv().token}`,
+            payloadTemplate: `{"notificationEmail": "${notificationEmail}", "eventType": {{eventType}}, "eventData": {{eventData}}, "resource": {{resource}} }`,
+        });
+    }
+
+    let dataByStates = {};
+    let lastUpdated = 'N/A';
+
+    const crawler = new Apify.BasicCrawler({
+        requestList,
+        handleRequestFunction: async ({request}) => {
+            let response;
+            let body;
+            let $;
+            let tableRows;
+            response = await requestAsBrowser({
+                url: request.url,
+                json: true,
+            });
+            body = response.body;
+            let countries = [];
+            let rows = body.rows;
+            rows.forEach(row => {
+                var country = countryName(row[1], 'en');
+                var coutryCode = row[1];
+                if (!countries.includes(coutryCode) && typeof(country) != 'undefined') countries.push(coutryCode);
+            });
+            countries.sort(function (a, b) {
+                a = countryName(a, 'en');
+                b = countryName(b, 'en');
+
+                if (a > b) {
+                    return 1;
+                }
+                if (b > a) {
+                    return -1;
+                }
+                return 0;
+            });
+            countries.forEach(countryCode => {
+                var country = countryName(countryCode, 'en');
+                var countryRows = rows.filter(function (row) {
+                    return row[1] == countryCode;
+                });
+                dataByStates[country] = [];
+                countryRows.forEach(row => {
+                    let data = {};
+                    data['date'] = new Date(row[0]).toISOString();
+                    data['deceased'] = parseInt(row[4]);
+                    data['deceasedNew'] = parseInt(row[3]);
+                    data['confirmed'] = parseInt(row[6]);
+                    data['confirmedNew'] = parseInt(row[5]);
+                    dataByStates[country].push(data);
+                });
+            });
+
+            lastUpdated = new Date(body.lastUpdateTime).toISOString();
+        }
+    });
+
+    log.info('CRAWLER -- start');
+    await crawler.run();
+    log.info('CRAWLER -- finish');
+
+    console.log(`Processing and saving data.`);
+
+    const now = new Date();
+
+    const data = {
+        dataByStates: dataByStates,
+        historyData: "",
+        sourceUrl: url,
+        lastUpdatedAtSource: lastUpdated,
+        lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
+        readMe: "https://apify.com/davidrychly/covid-who-sprinklr",
+    };
+
+    // Save to history
+    const latest = await kvStore.getValue(LATEST);
+    if (latest && latest.lastUpdatedAtApify) {
+        delete latest.lastUpdatedAtApify;
+    }
+    const actual = Object.assign({}, data);
+    delete actual.lastUpdatedAtApify;
+
+    if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+        await dataset.pushData(data);
+    }
+
+    await kvStore.setValue(LATEST, data);
+    await Apify.pushData(data);
+
+    console.log('Done.');
+});
