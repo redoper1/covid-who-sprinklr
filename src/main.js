9f74d50a9ca46d1aea4a79e5684f93e4445ac5d8
David Rychlý <davidrychly2@gmail.com>
2021-01-13 19:16:15 +0100

    Additional data scraping, added regions and country info

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ Apify.main(async () => {
         {
             url: 'https://covid19.who.int/page-data/index/page-data.json'
         }
-    ])
+    ]);
 
     if (notificationEmail) {
         await Apify.addWebhook({
@@ -24,6 +24,8 @@ Apify.main(async () => {
     }
 
     let dataByStates = {};
+    let dataByRegions = {};
+    let statesInfo = {};
     let lastUpdated = 'N/A';
 
     const crawler = new Apify.BasicCrawler({
@@ -32,17 +34,19 @@ Apify.main(async () => {
             let response;
             let body;
             let countryGroups;
+            let regionGroups;
             response = await requestAsBrowser({
                 url: request.url,
                 json: true,
             });
             body = response.body;
             countryGroups = body.result.pageContext.rawDataSets.countryGroups;
+            regionGroups = body.result.pageContext.rawDataSets.regionGroups;
             let countries = [];
             let countriesObj = [];
             countryGroups.forEach(countryObj => {
-                var country = countryName(countryObj.value, 'en');
-                var coutryCode = countryObj.value;
+                let country = countryName(countryObj.value, 'en');
+                let coutryCode = countryObj.value;
                 if (!countries.includes(coutryCode) && typeof(country) != 'undefined') {
                     countries.push(coutryCode);
                     countriesObj.push(coutryCode);
@@ -62,9 +66,21 @@ Apify.main(async () => {
                 return 0;
             });
             countries.forEach(countryCode => {
-                var country = countryName(countryCode, 'en');
-                var countryRows = countriesObj[countryCode].data.rows;
+                let country = countryName(countryCode, 'en');
+                let countryRows = countriesObj[countryCode].data.rows;
                 dataByStates[country] = [];
+                statesInfo[country] = [];
+
+                let countryInfo = {};
+                body.result.pageContext.rawDataSets.transmissionData.rows.forEach(row => {
+                    if (row.ISO_2_CODE === countryCode) {
+                        countryInfo['whoRegion'] = row.WHO_REGION;
+                        countryInfo['transmissionClassification'] = row.CLASSIFICATION;
+                        countryInfo['date'] = new Date(row.DATE).toISOString();
+                        statesInfo[country].push(countryInfo);
+                    }
+                });
+
                 countryRows.forEach(row => {
                     let data = {};
                     data['date'] = new Date(row[0]).toISOString();
@@ -72,8 +88,44 @@ Apify.main(async () => {
                     data['deceasedNew'] = parseInt(row[2]);
                     data['confirmed'] = parseInt(row[8]);
                     data['confirmedNew'] = parseInt(row[7]);
-
+                    data['deceasedLast7Days'] = parseInt(row[4]);
+                    data['deceasedLast7DaysChange'] = parseFloat(row[5]);
+                    data['deceasedPerMillion'] = parseFloat(row[6]);
+                    data['casesLast7Days'] = parseInt(row[9]);
+                    data['casesLast7DaysChange'] = parseFloat(row[10]);
+                    data['casesPerMillion'] = parseFloat(row[11]);
                     dataByStates[country].push(data);
+                });
+            });
+            let regions = [];
+            let regionsObj = [];
+            regionGroups.forEach(regionObj => {
+                let regionCode = regionObj.value;
+                if (!regions.includes(regionCode)) {
+                    regions.push(regionCode);
+                    regionsObj.push(regionCode);
+                    regionsObj[regionCode] = regionObj;
+                }
+            });
+            regions.sort();
+            regions.forEach(regionCode => {
+                let regionRows = regionsObj[regionCode].data.rows;
+                dataByRegions[regionCode] = [];
+
+                regionRows.forEach(row => {
+                    let data = {};
+                    data['date'] = new Date(row[0]).toISOString();
+                    data['deceased'] = parseInt(row[2]);
+                    data['deceasedNew'] = parseInt(row[1]);
+                    data['confirmed'] = parseInt(row[7]);
+                    data['confirmedNew'] = parseInt(row[6]);
+                    data['deceasedLast7Days'] = parseInt(row[3]);
+                    data['deceasedLast7DaysChange'] = parseFloat(row[4]);
+                    data['deceasedPerMillion'] = parseFloat(row[5]);
+                    data['casesLast7Days'] = parseInt(row[8]);
+                    data['casesLast7DaysChange'] = parseFloat(row[9]);
+                    data['casesPerMillion'] = parseFloat(row[10]);
+                    dataByRegions[regionCode].push(data);
                 });
             });
             lastUpdated = body.result.pageContext.rawDataSets.lastUpdate;
@@ -90,6 +142,8 @@ Apify.main(async () => {
 
     const data = {
         dataByStates: dataByStates,
+        dataByRegions: dataByRegions,
+        statesInfo: statesInfo,
         historyData: "https://api.apify.com/v2/datasets/4wrWtORugf0148gJ6/items?format=json&clean=1",
         sourceUrl: url,
         lastUpdatedAtSource: lastUpdated,
